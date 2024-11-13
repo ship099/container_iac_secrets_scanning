3d7fb80c9d8d4b63a830206405f0b925b8a1e619
Shipra Kumari <skumari@veracode.com>
2024-11-13 18:23:38 +0530

    Custom CLI path

--- src/install_cli.ts
+++ src/install_cli.ts
@@ -8,22 +8,22 @@ import path from "path";
 
 export async function install_cli(parameters: any) {
 
-  let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli; curl -fsS https://tools.veracode.com/veracode-cli/install | sh`
+  let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli;`
 
-
-  // const cliFileName = `veracode-cli_2.28.0_linux_x86`
-  // const cliFile = `${cliFileName}.tar.gz`;
-  // const downloadUrl = `https://tools.veracode.com/veracode-cli/${cliFile}`;
-  // const response = await axios.get(downloadUrl, { responseType: 'arraybuffer', maxContentLength: Number.MAX_SAFE_INTEGER, maxBodyLength: Number.MAX_SAFE_INTEGER });
-  // const buffer = Buffer.from(response.data);
-  // const cliPath = path.resolve('veracode-cli')
-  // const tempZipPath = path.resolve(cliPath, cliFile);
-  // fs.writeFileSync(tempZipPath, buffer);
-  // const command = `tar -xzf ${tempZipPath} -C ${cliPath}`
-  // let curlCommandOutput = execSync(command);
-  // fs.unlinkSync(tempZipPath);
   core.info('Install command :' + installCommand)
   let curlCommandOutput = execSync(installCommand)
+  const cliFileName = `veracode-cli_2.28.0_linux_x86`
+  const cliFile = `${cliFileName}.tar.gz`;
+  const downloadUrl = `https://tools.veracode.com/veracode-cli/${cliFile}`;
+  const response = await axios.get(downloadUrl, { responseType: 'arraybuffer', maxContentLength: Number.MAX_SAFE_INTEGER, maxBodyLength: Number.MAX_SAFE_INTEGER });
+  const buffer = Buffer.from(response.data);
+  const tempZipPath = path.resolve(cliFile);
+  console.log("tempZipFile", tempZipPath)
+  fs.writeFileSync(tempZipPath, buffer);
+  const command = `tar -xzf ${tempZipPath} -C ${__dirname}`
+  let curlCommandOutput1 = execSync(command);
+  fs.unlinkSync(tempZipPath);
+
 
   // const pwdCommand = `pwd`
   // //const lsCommand = `cat ${scaResult.fileName}`
@@ -40,7 +40,7 @@ export async function install_cli(parameters: any) {
   if (parameters.debug == "true") {
     core.info('#### DEBUG START ####')
     core.info('intall_cli.ts - command output')
-    core.info('command output : ' + curlCommandOutput)
+    core.info('command output : ' + curlCommandOutput1)
     core.info('#### DEBUG END ####')
   }
   core.info(`${curlCommandOutput}`)
