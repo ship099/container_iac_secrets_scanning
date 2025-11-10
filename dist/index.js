da84d1fe79cc0a4b9bc36afdc0ff29bc9d3bbbf7
Shipra Kumari <skumari@veracode.com>
2025-11-10 11:23:00 +0530

    catching the error

--- dist/index.js
+++ dist/index.js
@@ -9942,12 +9942,22 @@ function install_cli(parameters) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             //let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli; curl -fsS https://tools.veracode.com/veracode-cli/install | sh`
-            let installCommandInitial = `cd ..;mkdir veracode-cli; cd veracode-cli;`;
+            let installCommandInitial = `cd ..;`;
             // let installCommand = 'powershell -Command "Set-Location ..; New-Item -ItemType Directory -Force -Name veracode-cli; Set-Location veracode-cli; Invoke-WebRequest -Uri https://tools.veracode.com/veracode-cli/install.ps1 -OutFile install.ps1;"'
             // let makeDirCommand = execSync(installCommandInitial);
             const pwdCommand = `pwd`;
             //const lsCommand = `cat ${scaResult.fileName}`
             const lsCommand = `dir`;
+            try {
+                console.log("before executing pwd");
+                (0, child_process_1.execSync)(pwdCommand, { stdio: 'inherit' });
+                (0, child_process_1.execSync)(lsCommand, { stdio: 'inherit' });
+                console.log("after executing pwd");
+            }
+            catch (e) {
+                console.log("Shipra executing command", e);
+            }
+            let makeDirCommand = (0, child_process_1.execSync)(installCommandInitial);
             try {
                 console.log("before executing pwd");
                 (0, child_process_1.execSync)(pwdCommand, { stdio: 'inherit' });
--- src/install_cli.ts
+++ src/install_cli.ts
@@ -5,7 +5,7 @@ import { execSync,exec } from "child_process";
 export async function install_cli(parameters:any) {
 try{
     //let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli; curl -fsS https://tools.veracode.com/veracode-cli/install | sh`
-    let installCommandInitial = `cd ..;mkdir veracode-cli; cd veracode-cli;`
+    let installCommandInitial = `cd ..;`
    // let installCommand = 'powershell -Command "Set-Location ..; New-Item -ItemType Directory -Force -Name veracode-cli; Set-Location veracode-cli; Invoke-WebRequest -Uri https://tools.veracode.com/veracode-cli/install.ps1 -OutFile install.ps1;"'
   // let makeDirCommand = execSync(installCommandInitial);
 
@@ -22,6 +22,17 @@ try{
     console.log("Shipra executing command", e)
   }
 
+  let makeDirCommand = execSync(installCommandInitial);
+
+  try {
+    console.log("before executing pwd")
+    execSync(pwdCommand, { stdio: 'inherit' })
+    execSync(lsCommand, { stdio: 'inherit' })
+    console.log("after executing pwd")
+  }
+  catch (e) {
+    console.log("Shipra executing command", e)
+  }
    let installCommand =  `powershell -NoProfile -ExecutionPolicy Bypass -Command "
     Invoke-WebRequest -Uri https://tools.veracode.com/veracode-cli/install.ps1 -OutFile install.ps1
   "`
