90b55e1b7e12e26d0d2599ba1c1054075c7e3319
Shipra Kumari <skumari@veracode.com>
2025-11-12 12:34:16 +0530

    consoles

--- src/install_cli.ts
+++ src/install_cli.ts
@@ -56,13 +56,13 @@ try{
 //const psCommand = `(New-Object System.Net.WebClient).DownloadFile('https://tools.veracode.com/veracode-cli/install.ps1', '${brocolliDir}')`
 
 //this one is downloading the cli and keeping it in the destination path provided.
-const psCommand = `$destFile = Join-Path '${brocolliDir}' 'veracode'; Invoke-WebRequest -Uri 'https://tools.veracode.com/veracode-cli/install.ps1' -OutFile $destFile;& $destFile -DestinationPath '${brocolliDir}'`;
+//const psCommand = `$destFile = Join-Path '${brocolliDir}' 'veracode'; Invoke-WebRequest -Uri 'https://tools.veracode.com/veracode-cli/install.ps1' -OutFile $destFile;& $destFile -DestinationPath '${brocolliDir}'`;
 
 //const psCommand = `Invoke-WebRequest -Uri 'https://tools.veracode.com/veracode-cli/install.ps1' -OutFile $env:TEMP\\veracode; & $env:TEMP\\veracode scan --source ${workspace} --type directory --format json --output results.json`
 
 
 
-execSync(`powershell.exe -Command "${psCommand}"`, { stdio: 'inherit' });
+execSync(`powershell.exe -Command "${psCommand1}"`, { stdio: 'inherit' });
 
   console.log('Download complete!')
  // execSync(`powershell.exe -Command "${psCommand2}"`, { stdio: 'inherit' });
